b338d3cf1835d1aa053488106bed8bee25a472d5
RobertGlos <121170882+RobertGlos@users.noreply.github.com>
2024-09-21 17:08:46 +0200

    Update main.js

--- main.js
+++ main.js
@@ -27,6 +27,7 @@ async function getItemTable(){
 
     var resp = await fetch(CUSTOMITEMSAPI);
     var resp2 = await fetch(ITEMSAPI);
+    console.log("2")
     console.log(resp2)
     var json = await resp.json()
     for(item of Object.keys(json)) {
